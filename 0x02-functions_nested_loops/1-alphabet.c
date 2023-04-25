3c75d4f19ccafd213feaaa562af4edf446ac59ea
mspriscy <ms.priscillaokyere@gmail.com>
2023-04-25 07:07:05 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+char p;
+for (p = 'a'; p <= 'z'; p++)
+_putchar(p);
+_putchar('\n');
+}

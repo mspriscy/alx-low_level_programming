4cb34605bd6bf4cd7a8940a065b9c9d4c6650df0
mspriscy <ms.priscillaokyere@gmail.com>
2023-04-27 06:01:06 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int z;
+for (z = 1; z <= n; z++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}

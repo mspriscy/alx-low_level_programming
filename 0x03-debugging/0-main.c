a8f39cd2565afae4be916957c7d2dea8a74f2005
mspriscy <ms.priscillaokyere@gmail.com>
2023-04-27 16:04:30 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
-void positive_or_negative(int i)
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }

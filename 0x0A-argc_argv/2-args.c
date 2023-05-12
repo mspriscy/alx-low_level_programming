affa4aad9ac2998bcd06a6f63348d18e2120b959
mspriscy <ms.priscillaokyere@gmail.com>
2023-05-12 08:21:22 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - a program that prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int z;
+for (z = 0; z < argc; z++)
+{
+printf("%s\n", argv[z]);
+}
+return (0);
+}
